ba6fca4dd5f5239ef47ba931bb24a956df3565d4
Jacob Kelly <jskelly2021@gmail.com>
2024-05-21 14:42:42 -0500

    Added the hexagon struct to CategoryPanel file and made a custom initializer for easier use.

--- TaskTracker/Views/Home/CategoryPanel.swift
+++ TaskTracker/Views/Home/CategoryPanel.swift
@@ -7,34 +7,74 @@
 
 import SwiftUI
 
+struct Hexagon: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            let hOffset: CGFloat = rect.width * 0.3
+            let vOffset: CGFloat = rect.height * 0.3
+
+            path.move(to: CGPoint(x: rect.minX + hOffset, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX - hOffset, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY + vOffset))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY - vOffset))
+            path.addLine(to: CGPoint(x: rect.maxX - hOffset, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX + hOffset, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY - vOffset))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.minY + vOffset))
+            path.addLine(to: CGPoint(x: rect.minX + hOffset, y: rect.minY))
+        }
+    }
+}
+
 struct CategoryPanel: View {
     let name: String
-    let numTasks: Int
+    let icon: String?
+    let numTasks: Int?
     let color: Color
+    let dimension: CGFloat
+
+    init (name: String, icon: String? = nil, numTasks: Int? = nil, color: Color, dimension: CGFloat = 125.0) {
+        self.name = name
+        self.icon = icon
+        self.numTasks = numTasks
+        self.color = color
+        self.dimension = dimension
+    }
 
     var body: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: 8)
+            Hexagon()
                 .fill(color)
                 .frame(
-                    width: 150,
-                    height: 200)
+                    width: dimension,
+                    height: dimension)
                 .shadow(
                     color: .black,
-                    radius: 10,
-                    x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 10.0)
+                    radius: 11,
+                    x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 8.0)
                 .padding(.all, 10)
+
             VStack {
+                if let icon {
+                    Label("label", systemImage: "\(icon)")
+                        .labelStyle(.iconOnly)
+                        .font(.largeTitle)
+                }
+
                 Text(name)
-                    .font(.title)
+                    .minimumScaleFactor(0.4)
                     .bold()
-                Text("\(numTasks) tasks")
-                    .font(.subheadline)
+
+                if let numTasks {
+                    Text("\(numTasks) tasks")
+                        .font(.subheadline)
+                }
             }
         }
     }
 }
 
 #Preview {
-    CategoryPanel(name:"Priority", numTasks: 8, color: .red)
+    CategoryPanel(name: "Upcoming", numTasks: 8, color: .red)
+//    CategoryPanel(name: "Create", icon: "plus.circle", color: .gray)
 }
--- TaskTracker/Views/Home/CategoryView.swift
+++ TaskTracker/Views/Home/CategoryView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct CategoryView: View {
     var body: some View {
-        HStack {
-            CategoryPanel(name: "Priority", numTasks: 8, color: .yellow)
-            CategoryPanel(name: "Upcoming", numTasks: 13, color: .blue)
-        }
-        HStack {
-            CategoryPanel(name: "Groups", numTasks: 9, color: .purple)
-            CategoryPanel(name: "Overdue", numTasks: 3, color: .red)
+        VStack {
+            HStack {
+                CategoryPanel(name: "Priority", numTasks: 8, color: .yellow)
+                CategoryPanel(name: "Upcoming", numTasks: 13, color: .blue)
+            }
+
+            HStack {
+                CategoryPanel(name: "Create", icon: "plus.circle", color: .gray, dimension: 100)
+            }
+
+            HStack {
+                CategoryPanel(name: "Groups", numTasks: 9, color: .purple)
+                CategoryPanel(name: "Overdue", numTasks: 3, color: .red)
+            }
         }
     }
 }
