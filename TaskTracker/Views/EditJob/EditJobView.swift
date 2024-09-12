bfc194a6282e148979864e9f7e23a90e7b3db842
Jacob Kelly <jskelly2021@gmail.com>
2024-09-11 21:54:04 -0500

    Edit job view can now add a job

--- TaskTracker/Views/EditJob/EditJobView.swift
+++ TaskTracker/Views/EditJob/EditJobView.swift
@@ -8,11 +8,24 @@
 import SwiftUI
 
 struct EditJobView: View {
+    @Environment(\.managedObjectContext)
+    var context
+
+    @FetchRequest(sortDescriptors: [])
+    var jobs: FetchedResults<Job>
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: {
+            let job = Job(context: context)
+            job.title = "Test Job"
+            try? context.save()
+        }, label: {
+            Text("Add job")
+        })
     }
 }
 
 #Preview {
     EditJobView()
+        .environment(\.managedObjectContext, DataController().container.viewContext)
 }
