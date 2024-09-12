0064993c8f26d47aa7f876f94487bc1724b8da66
Jacob Kelly <jskelly2021@gmail.com>
2024-09-11 21:46:15 -0500

    Added a navigation link to navigate to editJob view

---
+++ TaskTracker/Views/EditJob/EditJobView.swift
@@ -0,0 +1,18 @@
+//
+//  EditJobView.swift
+//  TaskTracker
+//
+//  Created by Jacob Kelly on 9/11/24.
+//
+
+import SwiftUI
+
+struct EditJobView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditJobView()
+}
