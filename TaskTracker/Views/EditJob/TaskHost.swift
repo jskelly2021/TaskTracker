5264ae1a64bb0a508adc808f5cb919e2ec79c6e8
Jacob Kelly <jskelly2021@gmail.com>
2024-11-14 08:48:20 -0600

    Set up TaskHost and TaskSummary for use

---
+++ TaskTracker/Views/EditJob/TaskHost.swift
@@ -0,0 +1,30 @@
+//
+//  TaskViewHost.swift
+//  TaskTracker
+//
+//  Created by Jacob Kelly on 11/14/24.
+//
+
+import SwiftUI
+
+struct TaskHost: View {
+    @Environment(\.editMode) var editMode
+    @Environment(\.dismiss) var dismiss
+
+    var job: Job?
+
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                EditButton()
+            }
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    TaskHost()
+}
---
+++ TaskTracker/Views/EditJob/TaskSummary.swift
@@ -0,0 +1,18 @@
+//
+//  TaskSummaryView.swift
+//  TaskTracker
+//
+//  Created by Jacob Kelly on 11/14/24.
+//
+
+import SwiftUI
+
+struct TaskSummary: View {
+    var body: some View {
+        Rectangle()
+    }
+}
+
+#Preview {
+    TaskSummary()
+}
