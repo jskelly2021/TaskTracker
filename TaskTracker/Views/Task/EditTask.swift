2012ada288475b0696bce89a25fcaf018869fd8e
Jacob Kelly <jskelly2021@gmail.com>
2024-11-22 14:12:03 -0600

    Broke up EditTask into smaller functions

--- TaskTracker/Views/Task/EditTask.swift
+++ TaskTracker/Views/Task/EditTask.swift
@@ -15,34 +15,11 @@ struct EditTask: View {
 
     var body: some View {
         VStack(alignment: .leading, spacing: 10.0) {
-            TextField("Title", text: $job.title.fallback(""), axis: .vertical)
-                .lineLimit(3)
-                .font(.largeTitle)
-                .bold()
 
-            TextField("Details", text: $job.details.fallback(""), axis: .vertical)
-                .lineLimit(5)
-                .font(.headline)
-
+            jobDescription()
             Spacer()
-
-            HStack {
-                Text("Repeats")
-                    .font(.headline)
-
-                Picker("TimeScale", selection: $job.timeScale) {
-                    ForEach(TimeScales.allCases) { option in
-                        Text("\(option.description)")
-                            .tag(option.rawValue)
-                    }
-                }
-                .pickerStyle(.segmented)
-            }
-
-            DatePicker("Deadline", selection: $job.deadline.fallback(Date()), displayedComponents: .date)
-                .datePickerStyle(.graphical)
-                .id(job.deadline ?? Date())
-
+            timeScaleSelector()
+            deadlinePicker()
             Spacer()
 
             HStack {
@@ -53,6 +30,44 @@ struct EditTask: View {
         }
     }
 
+    // Title and Details
+    func jobDescription() -> some View {
+        VStack {
+            TextField("Title", text: $job.title.fallback(""), axis: .vertical)
+                .lineLimit(3)
+                .font(.largeTitle)
+                .bold()
+
+            TextField("Details", text: $job.details.fallback(""), axis: .vertical)
+                .lineLimit(5)
+                .font(.headline)
+        }
+    }
+
+    // Time Scale Picker
+    func timeScaleSelector() -> some View {
+        HStack {
+            Text("Repeats")
+                .font(.headline)
+
+            Picker("TimeScale", selection: $job.timeScale) {
+                ForEach(TimeScales.allCases) { option in
+                    Text("\(option.description)")
+                        .tag(option.rawValue)
+                }
+            }
+            .pickerStyle(.segmented)
+        }
+    }
+
+    // Date Picker
+    func deadlinePicker() -> some View {
+        DatePicker("Deadline", selection: $job.deadline.fallback(Date()), displayedComponents: .date)
+            .datePickerStyle(.graphical)
+            .id(job.deadline ?? Date())
+    }
+
+    // Delete Button
     func deleteButton() -> some View {
         Button {
             context.delete(job)
